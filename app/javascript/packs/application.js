e37aa329285415ee46a03fa143a7c61ceeac4844
Jose David <jose.camacho@ucrso.info>
2022-01-07 14:26:35 -0600

    Add Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+import "bootstrap"
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
