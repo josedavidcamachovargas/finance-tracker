7f1e5b4ca24d8479aba7ed28c4e6de72cd9edf70
Jose David <jose.camacho@ucrso.info>
2022-01-07 17:41:52 -0600

    Problema con credenciales

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,13 @@
 class Stock < ApplicationRecord
+
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new(publishable_token: 'Tpk_c24aebfa94f54940af03a06b1074193e',
+                                      endpoint: 'https://sandbox.iexapis.com/v1')
+        client.price(ticker_symbol)                                      
+    end
+
 end
+
+# iex_client:
+#   sandbox_api_key: "Tpk_c24aebfa94f54940af03a06b1074193e"
+#   client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
