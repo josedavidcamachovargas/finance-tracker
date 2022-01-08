94a3bfb09c0515239c500a9fb2139a50adb204b3
Jose David <jose.camacho@ucrso.info>
2022-01-07 22:58:55 -0600

    Setup UserStock resource

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
 
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(publishable_token: 'Tpk_c24aebfa94f54940af03a06b1074193e',
