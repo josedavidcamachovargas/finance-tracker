1d9787b9ef4361231f474bc4fee47858ed3f9f8a
Jose David <jose.camacho@ucrso.info>
2022-01-07 18:30:09 -0600

    Dealing with invalid search results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,7 +3,11 @@ class Stock < ApplicationRecord
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(publishable_token: 'Tpk_c24aebfa94f54940af03a06b1074193e',
                                       endpoint: 'https://sandbox.iexapis.com/v1')
-        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        begin
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+            return nil
+        end
     end
 
 end
