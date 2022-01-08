bc7043d34ee57f3480a15471c31d2d983b358d96
Jose David <jose.camacho@ucrso.info>
2022-01-07 19:34:08 -0600

    Setup JavaScript response

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@ import "bootstrap"
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
 require("jquery")
 
 /* eslint no-console:0 */
